a8b1ce86cf652212817e69f89b61c4f49647185a
rabuhassan <103109080+rabuhassan@users.noreply.github.com>
2022-06-01 21:29:51 +0200

    Practical Activity 1.2.5

---
+++ my_first_code.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1.2.5 Practical activity 
+# ## Write your first line of code
+
+# In[1]:
+
+
+"""Using the data table provided by Lindstrom & Thomos, find: 
+    - find the total sales for the month
+    - calculate the average sales price for the month 
+    - find the missing value from the last column of the table.
+    """
+
+
+# In[2]:
+
+
+# This is my first line of code. 
+print("My name is Rachid Abu-Hassan")
+print("The date is June 1, 2022")
+
+
+# In[3]:
+
+
+#Write a program to analyse real estate data
+# Sales prices for the properties. 
+A = 45000
+B = 23400
+C = 67000
+D = 34600
+E = 12900
+
+
+# In[11]:
+
+
+Total_Sales = A + B + C + D + E
+Average = Total_Sales/5
+Price_C = C - 6500
+
+print("The Total Sales for the Month of December is:", Total_Sales)
+print("The Average Sales Price for the Month of December is:", Average)
+print("The Price of Property C is:", Price_C)
+
+
+# In[ ]:
+
+
+
+
